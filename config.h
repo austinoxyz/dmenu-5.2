9c8a1405bf113eec06917f161c9b97d5056a1d67
Austin O'Donnell <aodonnell2536.3@gmail.com>
2023-04-10 23:52:01 -0400

    add fuzzy highlighting

--- config.h
+++ config.h
@@ -15,6 +15,8 @@ static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#839496", "#002b36" },
 	[SchemeSel] = { "#839496", "#073642"},
+	[SchemeNormHighlight] = { "#ffc978", "#002b36" },
+	[SchemeSelHighlight] = { "#e20000", "#073642" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
